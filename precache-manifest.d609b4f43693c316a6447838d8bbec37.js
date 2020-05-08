22dcae22bbc59c59d0a138a581b2663e8bfa3ea8
John Desrosiers <john.desro@gmail.com>
2020-05-07 17:52:55 -0700

    Updates

---
+++ precache-manifest.d609b4f43693c316a6447838d8bbec37.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d667fe3631c0508f22b7e5235942577e",
+    "url": "/lotr-characters/index.html"
+  },
+  {
+    "revision": "4fa8f1614b72895b788c",
+    "url": "/lotr-characters/static/js/2.27d07ab5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lotr-characters/static/js/2.27d07ab5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5abd49e9d432269deacb",
+    "url": "/lotr-characters/static/js/main.30a62da2.chunk.js"
+  },
+  {
+    "revision": "b49cf8adcc9ebb9c85ad",
+    "url": "/lotr-characters/static/js/runtime-main.8351d1d5.js"
+  }
+]);
